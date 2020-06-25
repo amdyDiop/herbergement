96feac7b1a63701098a6ee5849a9a630d555b5c5
dofbi <amdymila@gmail,com>
2020-06-25 02:06:50 +0000

    integration maquette

---
+++ template/listEtudiant.php
@@ -0,0 +1,104 @@
+<div class="col-sm-12">
+    <table id="example2" class="table table-bordered table-hover dataTable" role="grid"
+           aria-describedby="example2_info">
+        <thead>
+        <tr role="row">
+            <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-sort="ascending"
+                aria-label="Rendering engine: activate to sort column descending">ID
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Browser: activate to sort column ascending">Prénom
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Platform(s): activate to sort column ascending">Nom
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">Matricule
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">Date_naiss
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">chambre
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">Bourse
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr role="row" class="odd">
+            <td class="sorting_1">Gecko</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+        </tr>
+        <tr role="row" class="odd">
+            <td class="sorting_1">Gecko</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+        </tr>
+        <tr role="row" class="odd">
+            <td class="sorting_1">Gecko</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+        </tr>
+        <tr role="row" class="odd">
+            <td class="sorting_1">Gecko</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+        </tr>
+        <tr role="row" class="odd">
+            <td class="sorting_1">Gecko</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+            <td>Firefox 1.0</td>
+            <td>Win 98+ / OSX.2+</td>
+            <td>1.7</td>
+        </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+            <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-sort="ascending"
+                aria-label="Rendering engine: activate to sort column descending">ID
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Browser: activate to sort column ascending">Prénom
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Platform(s): activate to sort column ascending">Nom
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">Matricule
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">Date_naiss
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">chambre
+            </th>
+            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1"
+                aria-label="Engine version: activate to sort column ascending">Bourse
+            </th>
+        </tr>
+        </tfoot>
+    </table>
+</div>
